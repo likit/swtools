d7a2a330ead2cbb1d244b6c76cd88a34d52e22da
Likit Preeyanon <preeyano@msu.edu>
2015-03-27 14:37:24 +0700

    added assertions

--- tools.py
+++ tools.py
@@ -1,5 +1,7 @@
 def get_bmi(weight, height):
 	weight = float(weight)
 	height = float(height)
+	assert weight > 30, "Invalid weight for adults"
+	assert height > 1.0, "Invalid height for adults"
 	bmi = weight/(height)**2
 	return bmi
